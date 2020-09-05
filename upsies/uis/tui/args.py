969929df4dd4bf1577a4295f65004048fa415a16
Anonymous <you@example.com>
2020-09-05 15:09:35 +0200

    Remove metavar='' from add_subparsers() arguments

--- upsies/uis/tui/args.py
+++ upsies/uis/tui/args.py
@@ -31,7 +31,7 @@ def parse(args):
                         help='Tracker name',
                         default=None)
 
-    subparsers = parser.add_subparsers(title='subcommands', metavar='')
+    subparsers = parser.add_subparsers(title='subcommands')
 
     release_name = subparsers.add_parser(
         'release-name', aliases=('rn',), help='Create normalized release name',
