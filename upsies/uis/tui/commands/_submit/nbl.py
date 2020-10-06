f049fbc0e8c01dc1469eaed59be44dc465812015
plotski <plotski@example.org>
2020-10-06 11:54:44 +0200

    Add category job to submit.nbl

--- upsies/uis/tui/commands/_submit/nbl.py
+++ upsies/uis/tui/commands/_submit/nbl.py
@@ -1,5 +1,5 @@
 from ..... import jobs as _jobs
-from .....utils import cache, fs
+from .....utils import cache, fs, guessit
 from ._base import SubmitCommandBase
 
 import logging  # isort:skip
@@ -13,6 +13,7 @@ class submit(SubmitCommandBase):
             self.create_torrent_job,
             self.mediainfo_job,
             self.tvmaze_job,
+            self.category_job,
             self.submission_job,
             self.add_torrent_job,
             self.copy_torrent_job,
@@ -24,6 +25,16 @@ class submit(SubmitCommandBase):
             self.create_torrent_job,
             self.mediainfo_job,
             self.tvmaze_job,
+            self.category_job,
+        )
+
+    @cache.property
+    def mediainfo_job(self):
+        return _jobs.mediainfo.MediainfoJob(
+            homedir=fs.projectdir(self.args.CONTENT),
+            ignore_cache=self.args.ignore_cache,
+            content_path=self.args.CONTENT,
+            quiet=True,
         )
 
     @cache.property
@@ -36,10 +47,17 @@ class submit(SubmitCommandBase):
         )
 
     @cache.property
-    def mediainfo_job(self):
-        return _jobs.mediainfo.MediainfoJob(
+    def category_job(self):
+        # Season or Episode
+        if guessit.guessit(self.args.CONTENT).get('type') == 'episode':
+            guessed = 'Episode'
+        else:
+            guessed = 'Season'
+        return _jobs.prompt.ChoiceJob(
+            name='category',
+            label='Category',
             homedir=fs.projectdir(self.args.CONTENT),
             ignore_cache=self.args.ignore_cache,
-            content_path=self.args.CONTENT,
-            quiet=True,
+            choices=('Season', 'Episode'),
+            focused=guessed,
         )
