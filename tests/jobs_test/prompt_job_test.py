60fbe39d712b54809ce5a2ad4351e3ddc8e9a037
plotski <plotski@example.org>
2020-10-07 17:00:49 +0200

    Add more prompt.ChoiceJob tests

--- tests/jobs_test/prompt_job_test.py
+++ tests/jobs_test/prompt_job_test.py
@@ -1,4 +1,55 @@
+import re
+import pytest
+
 from upsies.jobs import prompt
+
+
+def test_Choice_choices_argument_is_shorter_than_two(tmp_path):
+    choices = ('foo',)
+    with pytest.raises(ValueError, match=(r'^choices must contain at least 2 items: '
+                                          rf'{re.escape(str(choices))}$')):
+        job = prompt.ChoiceJob(
+            homedir=tmp_path,
+            ignore_cache=True,
+            name='foo',
+            label='Foo',
+            choices=choices,
+        )
+
+def test_Choice_focused_argument_is_None(tmp_path):
+    choices = ('foo', 'bar', 'baz')
+    job = prompt.ChoiceJob(
+        homedir=tmp_path,
+        ignore_cache=True,
+        name='foo',
+        label='Foo',
+        choices=choices,
+    )
+    assert job.focused == 'foo'
+
+def test_Choice_focused_argument_is_valid(tmp_path):
+    choices = ('foo', 'bar', 'baz')
+    job = prompt.ChoiceJob(
+        homedir=tmp_path,
+        ignore_cache=True,
+        name='foo',
+        label='Foo',
+        choices=choices,
+        focused='bar',
+    )
+    assert job.focused == 'bar'
+
+def test_Choice_focused_argument_is_in(tmp_path):
+    choices = ('foo', 'bar', 'baz')
+    with pytest.raises(ValueError, match=(r'^Invalid choice: asdf$')):
+        job = prompt.ChoiceJob(
+            homedir=tmp_path,
+            ignore_cache=True,
+            name='foo',
+            label='Foo',
+            choices=choices,
+            focused='asdf',
+        )
 
 
 def test_Choice_properties(tmp_path):
@@ -14,6 +65,7 @@ def test_Choice_properties(tmp_path):
     assert job.label == 'Foo'
     assert job.choices == ('1', '2', '3')
     assert job.focused == '2'
+
 
 def test_Choice_choice_selected(tmp_path):
     job = prompt.ChoiceJob(
