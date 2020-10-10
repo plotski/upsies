9fd6cd798b3f5882f6c9106c401f48aadc887bad
plotski <plotski@example.org>
2020-10-10 18:13:45 +0200

    Export imdb.country()

--- upsies/tools/dbs/imdb/__init__.py
+++ upsies/tools/dbs/imdb/__init__.py
@@ -2,5 +2,5 @@ label = 'IMDb'
 _url_base = 'http://imdb.com'
 
 from .._common import info
-from ._info import summary, title_english, title_original, type, year
+from ._info import country, summary, title_english, title_original, type, year
 from ._search import search
