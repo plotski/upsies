51b66640f7035333681b1fe2950bfff04ef67ecc
plotski <plotski@example.org>
2020-10-17 17:27:32 +0200

    Make exceptions comparable for tests

---
+++ tests/errors_test.py
@@ -0,0 +1,24 @@
+import pytest
+
+from upsies import errors
+
+
+def error_classes():
+    clses = []
+    for name in dir(errors):
+        if not name.startswith('_'):
+            cls = getattr(errors, name)
+            print('name:', name, 'cls:', cls)
+            if isinstance(cls, type) and issubclass(cls, Exception):
+                clses.append(cls)
+    return clses
+
+
+@pytest.mark.parametrize(
+    argnames='cls',
+    argvalues=error_classes(),
+)
+def test_equality(cls):
+    assert cls('foo') == cls('foo')
+    assert cls('foo') != cls('bar')
+    assert cls('foo') != ValueError('foo')
--- upsies/errors.py
+++ upsies/errors.py
@@ -1,52 +1,58 @@
-class CancelledError(Exception):
+class UpsiesError(Exception):
+    """Base class for all exceptions raised by upsies"""
+
+    def __eq__(self, other):
+        if isinstance(other, type(self)) and str(other) == str(self):
+            print(str(other), repr(other), '==', str(self), repr(self))
+            return True
+        else:
+            return NotImplemented
+
+class CancelledError(UpsiesError):
     """User cancelled an operation"""
     pass
 
-class ConfigError(Exception):
+class ConfigError(UpsiesError):
     """Error while reading from a config file"""
     pass
 
-class DependencyError(Exception):
+class DependencyError(UpsiesError):
     """Some external tool is missing (e.g. mediainfo)"""
     pass
 
-class MediainfoError(Exception):
+class MediainfoError(UpsiesError):
     """Getting mediainfo failed"""
     pass
 
-class NoContentError(Exception):
+class NoContentError(UpsiesError):
     """No usable content found (e.g. no video files in the given path)"""
     pass
 
-class ProcessError(Exception):
+class ProcessError(UpsiesError):
     """Executing subprocess failed"""
     pass
 
-class PermissionError(Exception):
+class PermissionError(UpsiesError):
     """Insufficient permission (e.g. when reading a file)"""
     pass
 
-class RequestError(Exception):
+class RequestError(UpsiesError):
     """Network request failed"""
     pass
 
-class ScreenshotError(Exception):
+class ScreenshotError(UpsiesError):
     """Screenshot creation failed"""
-    def __init__(self, msg, video_file, timestamp):
-        super().__init__(f'{video_file}: Failed to create screenshot at {timestamp}: {msg}')
+    def __init__(self, msg, video_file=None, timestamp=None):
+        if not video_file or not timestamp:
+            super().__init__(msg)
+        else:
+            super().__init__(f'{video_file}: Failed to create screenshot at {timestamp}: {msg}')
 
-class SubprocessError(Exception):
-    """
-    Exception from a subprocess
-
-    The original traceback from within the subprocess is available as
-    :attr:`original_traceback`.
-    """
-    def __new__(cls, exception, original_traceback):
-        self = exception
-        self.original_traceback = f'Subprocess traceback:\n{original_traceback.strip()}'
-        return self
-
-class TorrentError(Exception):
+class TorrentError(UpsiesError):
     """Torrent file creation failed"""
     pass
+
+def SubprocessError(exception, original_traceback):
+    """Attach `original_traceback` to `exception`"""
+    exception.original_traceback = f'Subprocess traceback:\n{original_traceback.strip()}'
+    return exception
