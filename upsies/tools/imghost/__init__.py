e4f4672e1a8faf8af5e2111b5a2e23ea80eec403
plotski <plotski@example.org>
2020-11-03 13:01:06 +0100

    Lint

--- upsies/tools/imghost/__init__.py
+++ upsies/tools/imghost/__init__.py
@@ -5,4 +5,4 @@ Common API for image hosting services
 # isort:skip_file
 from ._base import UploaderBase
 from ._common import UploadedImage
-from . import (dummy, imgbox)
+from . import dummy, imgbox
