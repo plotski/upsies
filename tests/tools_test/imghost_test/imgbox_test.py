32bea285ed7c0eb9dce08dc8d81eb90cf2f0c49f
plotski <plotski@example.org>
2020-11-04 16:58:52 +0100

    Fix tools.imghost.imgbox tests for Python < 3.8

--- tests/tools_test/imghost_test/imgbox_test.py
+++ tests/tools_test/imghost_test/imgbox_test.py
@@ -6,6 +6,18 @@ from upsies import errors
 from upsies.tools.imghost import imgbox
 
 
+# FIXME: The AsyncMock class from Python 3.8 is missing __await__(), making it
+# not a subclass of typing.Awaitable.
+class AsyncMock(Mock):
+    def __call__(self, *args, **kwargs):
+        async def coro(_sup=super()):
+            return _sup.__call__(*args, **kwargs)
+        return coro()
+
+    def __await__(self):
+        return self().__await__()
+
+
 @patch('pyimgbox.Gallery')
 def test_thumb_width_argument_not_given(Gallery_mock, tmp_path):
     imgbox.Uploader(cache_dir=tmp_path)
@@ -25,9 +37,9 @@ def test_thumb_width_argument_given(Gallery_mock, tmp_path):
     )]
 
 
-@patch('pyimgbox.Gallery.upload')
 @pytest.mark.asyncio
-async def test_upload_handles_success(upload_mock, tmp_path):
+async def test_upload_handles_success(tmp_path, mocker):
+    upload_mock = mocker.patch('pyimgbox.Gallery.upload', AsyncMock())
     uploader = imgbox.Uploader(thumb_width=200, cache_dir=tmp_path)
     upload_mock.return_value = Mock(
         success=True,
@@ -44,9 +56,9 @@ async def test_upload_handles_success(upload_mock, tmp_path):
         'edit_url': 'http://foo.edit.url',
     }
 
-@patch('pyimgbox.Gallery.upload')
 @pytest.mark.asyncio
-async def test_upload_handles_error(upload_mock, tmp_path):
+async def test_upload_handles_error(tmp_path, mocker):
+    upload_mock = mocker.patch('pyimgbox.Gallery.upload', AsyncMock())
     uploader = imgbox.Uploader(thumb_width=200, cache_dir=tmp_path)
     upload_mock.return_value = Mock(
         success=False,
