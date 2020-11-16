0fc263feeb6c94371157eaccf10e762e75d8279b
plotski <plotski@example.org>
2020-11-16 13:37:29 +0100

    Display "Analyzing <content_name> ..." until screenshot creation begins

--- upsies/uis/tui/jobwidgets/_screenshots.py
+++ upsies/uis/tui/jobwidgets/_screenshots.py
@@ -1,3 +1,8 @@
+import os
+
+from prompt_toolkit.filters import Condition
+from prompt_toolkit.layout.containers import ConditionalContainer, HSplit
+
 from ....utils import cache
 from .. import widgets
 from . import JobWidgetBase
@@ -8,14 +13,28 @@ _log = logging.getLogger(__name__)
 
 class ScreenshotsJobWidget(JobWidgetBase):
     def setup(self):
+        content_name = os.path.basename(self.job.kwargs['content_path'])
+        self._status_text = widgets.TextField(f'Analyzing {content_name} ...')
         self._screenshot_progress = widgets.ProgressBar()
         self.job.on_output(self.handle_screenshot_path)
         self.job.on_finished(lambda _: self.invalidate())
 
     def handle_screenshot_path(self, path):
-        self._screenshot_progress.percent = self.job.screenshots_created / self.job.screenshots_total * 100
-        self.invalidate()
+        if self.job.screenshots_total > 0:
+            self._screenshot_progress.percent = self.job.screenshots_created / self.job.screenshots_total * 100
+            self.invalidate()
 
     @cache.property
     def runtime_widget(self):
-        return self._screenshot_progress
+        return HSplit(
+            children=[
+                ConditionalContainer(
+                    filter=Condition(lambda: not self.job.video_file),
+                    content=self._status_text,
+                ),
+                ConditionalContainer(
+                    filter=Condition(lambda: self.job.video_file),
+                    content=self._screenshot_progress,
+                ),
+            ],
+        )
