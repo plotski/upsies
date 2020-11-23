7122e9c295bae3ca2a3e133680a0b77224dcb2f4
plotski <plotski@example.org>
2020-11-23 12:40:33 +0100

    Use "dummy" image host for "dummy" tracker

--- upsies/defaults.py
+++ upsies/defaults.py
@@ -26,6 +26,7 @@ config = {
             base_url='http://localhost',
             source='DMY',
             announce='http://localhost:12345/dummy/announce',
+            image_host='dummy',
         ),
         'nbl': _TrackerConfig(
             base_url=base64.b64decode('aHR0cHM6Ly9uZWJ1bGFuY2UuaW8=').decode('ascii'),
