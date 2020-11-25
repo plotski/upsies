98565d06a733c336c9401a6ee3237098baa5b522
plotski <plotski@example.org>
2020-11-25 12:17:19 +0100

    Don't cache output of "set" command

--- upsies/jobs/config.py
+++ upsies/jobs/config.py
@@ -14,6 +14,7 @@ class SetJob(JobBase):
     name = 'set'
     label = 'Set'
     hidden = True
+    cache_file = None
 
     def initialize(self, *, config, option, value=None, reset=False):
         if value and reset:
