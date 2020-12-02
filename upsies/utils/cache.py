6aec2ff103cb2c205c1d110018463a92df3f097e
plotski <plotski@example.org>
2020-12-02 11:30:58 +0100

    Use functools.cached_property as utils.cache.property if possible

--- upsies/utils/cache.py
+++ upsies/utils/cache.py
@@ -1,26 +1,21 @@
-import functools
+try:
+    from functools import cached_property as property
+except ImportError:
+    import functools
 
-# https://github.com/jackmaney/lazy-property/
+    # https://github.com/jackmaney/lazy-property/
+    class property():
+        """Property that replaces itself with its return value on first access"""
 
-class property:
-    """Property that is only computed once per instance"""
+        def __init__(self, fget):
+            self.fget = fget
+            functools.update_wrapper(self, fget)
+            self.fset = None
 
-    def __init__(self, fget):
-        self.fget = fget
-        functools.update_wrapper(self, fget)
-        self.fset = None
-
-    @property
-    def setter(self):
-        def setter(fset):
-            self.fset = fset
-            functools.update_wrapper(self, fset)
-        return setter
-
-    def __get__(self, obj, cls):
-        if obj is None:
-            return self
-        else:
-            value = self.fget(obj)
-            setattr(obj, self.fget.__name__, value)
-            return value
+        def __get__(self, obj, cls):
+            if obj is None:
+                return self
+            else:
+                value = self.fget(obj)
+                setattr(obj, self.fget.__name__, value)
+                return value
