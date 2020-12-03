91f626b2da9e1c2e6a9c506639ba303c3e92dfef
plotski <plotski@example.org>
2020-12-03 15:48:35 +0100

    Order utils tests

--- tests/utils_test/utils_test.py
+++ tests/utils_test/utils_test.py
@@ -3,6 +3,16 @@ import pytest
 from upsies import utils
 
 
+def test_closest_number():
+    numbers = (10, 20, 30)
+    for n in (-10, 0, 9, 10, 11, 14):
+        assert utils.closest_number(n, numbers) == 10
+    for n in (16, 19, 20, 21, 24):
+        assert utils.closest_number(n, numbers) == 20
+    for n in range(26, 50):
+        assert utils.closest_number(n, numbers) == 30
+
+
 def test_pretty_bytes():
     assert utils.pretty_bytes(1023) == '1023 B'
     assert utils.pretty_bytes(1024) == '1.00 KiB'
@@ -12,16 +22,6 @@ def test_pretty_bytes():
     assert utils.pretty_bytes((1024**3) * 123) == '123.00 GiB'
     assert utils.pretty_bytes((1024**4) * 456) == '456.00 TiB'
     assert utils.pretty_bytes((1024**5) * 456) == '456.00 PiB'
-
-
-def test_closest_number():
-    numbers = (10, 20, 30)
-    for n in (-10, 0, 9, 10, 11, 14):
-        assert utils.closest_number(n, numbers) == 10
-    for n in (16, 19, 20, 21, 24):
-        assert utils.closest_number(n, numbers) == 20
-    for n in range(26, 50):
-        assert utils.closest_number(n, numbers) == 30
 
 
 def test_CaseInsensitiveString_equality():
