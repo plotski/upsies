a46b725254657aa468c8b9299ccc5ec38a6849ee
plotski <plotski@example.org>
2021-01-01 19:03:57 +0100

    Don't set empty status text in SubmitJobWidget

--- upsies/uis/tui/jobwidgets/_submit.py
+++ upsies/uis/tui/jobwidgets/_submit.py
@@ -10,9 +10,7 @@ _log = logging.getLogger(__name__)
 
 class SubmitJobWidget(JobWidgetBase):
     def setup(self):
-        self._status_info = widgets.TextField(
-            text='',
-        )
+        self._status_info = widgets.TextField()
         messages = (
             ('logging_in', 'Logging in...'),
             ('logged_in', 'Logged in successfully.'),
