915119fc5424e5a3e9da30a31efbeb9a566a6d4a
plotski <plotski@example.org>
2021-02-01 19:12:49 +0100

    Add SceneDbApiBase.is_scene_release()

--- tests/utils_test/scene_test/scene_base_test.py
+++ tests/utils_test/scene_test/scene_base_test.py
@@ -3,6 +3,7 @@ from unittest.mock import Mock, call
 import pytest
 
 from upsies import errors
+from upsies.utils.release import ReleaseInfo
 from upsies.utils.scene import base, predb
 
 
@@ -60,3 +61,48 @@ def test_normalize_results(testdb):
 @pytest.mark.asyncio
 async def test_is_scene_group(group, exp_return_value, store_response):
     assert await predb.PreDbApi().is_scene_group(group) is exp_return_value
+
+
+@pytest.mark.parametrize(
+    argnames='release_name, exp_return_value',
+    argvalues=(
+        # Movie: Abbreviated file in properly named directory
+        ('Dellamorte.Dellamore.1994.1080p.BluRay.x264-LiViDiTY/ly-dellmdm1080p.mkv', True),
+        ('Dellamorte.Dellamore.1994.1080p.Blu-ray.x264-LiViDiTY/ly-dellmdm1080p.mkv', True),
+        ('dellamorte.dellamore.1994.1080p.blu-ray.x264-lividity/ly-dellmdm1080p.mkv', True),
+
+        # Movie: Properly named file
+        ('Dellamorte.Dellamore.1994.1080p.BluRay.x264-LiViDiTY.mkv', True),
+
+        # Movie: Properly named file in properly named directory
+        ('Dellamorte.Dellamore.1994.1080p.BluRay.x264-LiViDiTY/Dellamorte.Dellamore.1994.1080p.BluRay.x264-LiViDiTY.mkv', True),
+
+        # Movie: Abbreviated file without properly named parent directory
+        ('path/to/ly-dellmdm1080p.mkv', None),
+
+        # Movie: Properly named file in lower-case
+        ('dellamorte.dellamore.1994.1080p.blu-ray.x264-lividity.mkv', True),
+
+        # Series: Scene released season pack
+        ('Bored.to.Death.S01.EXTRAS.720p.BluRay.x264-iNGOT', True),
+        ('bored.to.death.s01.720p.bluray.x264-ingot', True),
+        ('Bored.to.Death.S01.Jonathan.Amess.Brooklyn.720p.BluRay.x264-iNGOT.mkv', True),
+        ('bored.to.death.s01.720p.bluray.x264-ingot.mkv', True),
+
+        # Series: Scene released single episodes
+        ('Justified.S04E01.720p.BluRay.x264-REWARD', True),
+        ('Justified.S04E99.720p.BluRay.x264-REWARD', False),
+        ('Justified.S04.720p.BluRay.x264-REWARD', True),
+        ('Justified.S02.720p.BluRay.x264-REWARD', False),  # REWARD only released S01 + S04
+        ('Justified.720p.BluRay.x264-REWARD', False),
+
+        # Non-scene release
+        ('Rampart.2011.1080p.Bluray.DD5.1.x264-DON.mkv', False),
+        ('Damnation.S01.720p.AMZN.WEB-DL.DDP5.1.H.264-AJP69', False),
+        ('Damnation.S01E03.One.Penny.720p.AMZN.WEB-DL.DD+5.1.H.264-AJP69.mkv', False),
+    ),
+)
+@pytest.mark.asyncio
+async def test_is_scene_release(release_name, exp_return_value, store_response):
+    release = ReleaseInfo(release_name)
+    assert await predb.PreDbApi().is_scene_release(release) is exp_return_value
--- upsies/utils/scene/base.py
+++ upsies/utils/scene/base.py
@@ -55,3 +55,87 @@ class SceneDbApiBase(abc.ABC):
     async def is_scene_group(self, group):
         """Whether `group` is a scene group"""
         return bool(await self.search(group=str(group)))
+
+    async def is_scene_release(self, release):
+        """
+        Check if `release` is a scene release or not
+
+        :param release: Release name or path to release content
+
+        :return: `True`, `False` or `None` (unknown)
+        """
+        results = await self._search_with_series_info(release)
+        if results:
+            if self._release_is_in_results(release, results):
+                return True
+
+        # If we search for season pack but single episodes were released,
+        # search without "S[0-9]+" to find the episode releases.
+        elif release['type'] is _release.ReleaseType.season:
+            results = await self._search_without_series_info(release)
+            if results:
+                if self._release_is_in_results(release, results):
+                    return True
+
+        # If this is a file like "abd-mother.mkv" without a properly named
+        # parent directory, it's probably a scene release, but we can't say for
+        # sure.
+        from . import is_abbreviated_filename
+        if is_abbreviated_filename(release.path):
+            return None
+
+        return False
+
+    def _release_is_in_results(self, release, results):
+        results = [_release.ReleaseInfo(r) for r in results]
+
+        def releases_equal(r1, r2, cmp_episodes=True):
+            # Compare only some information in case `release` comes from a
+            # renamed release. We don't validate the release name here, only
+            # check if the typical scene release information matches our input.
+            if cmp_episodes:
+                keys = ('title', 'year', 'season', 'episode', 'resolution',
+                        'source', 'video_codec', 'group')
+            else:
+                keys = ('title', 'year', 'season', 'resolution',
+                        'source', 'video_codec', 'group')
+            for key in keys:
+                if r1[key].casefold() != r2[key].casefold():
+                    _log.debug('%s: Not a scene release because %r != %r',
+                               release.path, r1[key], r2[key])
+                    return False
+            return True
+
+        # Try to find the exact release
+        for result in results:
+            if releases_equal(release, result, cmp_episodes=True):
+                return True
+
+        # If we can't find the exact release, we might be looking at a season
+        # pack that was originally released as individual episodes. Searching
+        # for "S04" doesn't yield any results for "S04E01/2/3/..." releases.
+        if release['season'] and not release['episode']:
+            for result in results:
+                if releases_equal(release, result, cmp_episodes=False):
+                    return True
+
+        return False
+
+    async def _search_with_series_info(self, release):
+        query = [x for x in (release['title'],
+                             release['year'],
+                             release.season_and_episode,
+                             release['resolution'],
+                             release['source'],
+                             release['video_codec'])
+                 if x]
+        return await self.search(*query, group=release['group'])
+
+    async def _search_without_series_info(self, release):
+        query = [x for x in (release['title'],
+                             release['year'],
+                             release['resolution'],
+                             release['source'],
+                             release['video_codec'])
+                 if x]
+        return await self.search(*query, group=release['group'])
