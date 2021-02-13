cd3ceb06c3d96c6989a19431bcf9be694d6dccc0
plotski <plotski@example.org>
2021-02-13 13:04:55 +0100

    Add scene.release_files()

--- tests/utils_test/scene_test/scene_test.py
+++ tests/utils_test/scene_test/scene_test.py
@@ -90,3 +90,98 @@ def test_scenedb_fails_to_find_scenedb(mocker):
 async def test_is_scene_release(release_name, exp_return_value, store_response):
     assert await scene.is_scene_release(release_name) is exp_return_value
     assert await scene.is_scene_release(ReleaseInfo(release_name)) is exp_return_value
+
+
+@pytest.mark.parametrize(
+    argnames='release_name, exp_return_value',
+    argvalues=(
+        # Looking for single-file release when original release was single-file
+        ('Dellamorte.Dellamore.1994.1080p.BluRay.x264-LiViDiTY',
+         {'ly-dellmdm1080p.mkv': {'release_name': 'Dellamorte.Dellamore.1994.1080p.BluRay.x264-LiViDiTY',
+                                  'file_name': 'ly-dellmdm1080p.mkv',
+                                  'size': 7044061767,
+                                  'crc': 'B59CE234'}}),
+
+        # Looking for multi-file release when original release was multi-file
+        ('Bored.to.Death.S01.EXTRAS.720p.BluRay.x264-iNGOT',
+         {'Bored.to.Death.S01.Jonathan.Amess.Brooklyn.720p.BluRay.x264-iNGOT.mkv':
+          {'release_name': 'Bored.to.Death.S01.EXTRAS.720p.BluRay.x264-iNGOT',
+           'file_name': 'Bored.to.Death.S01.Jonathan.Amess.Brooklyn.720p.BluRay.x264-iNGOT.mkv',
+           'size': 518652629,
+           'crc': '497277ED'},
+          'Bored.to.Death.S01.Making.of.720p.BluRay.x264-iNGOT.mkv':
+          {'release_name': 'Bored.to.Death.S01.EXTRAS.720p.BluRay.x264-iNGOT',
+           'file_name': 'Bored.to.Death.S01.Making.of.720p.BluRay.x264-iNGOT.mkv',
+           'size': 779447228,
+           'crc': '8C8C0AE7'},
+          'Bored.to.Death.S01E03.Deleted.Scene.720p.BluRay.x264-iNGOT.mkv':
+          {'release_name': 'Bored.to.Death.S01.EXTRAS.720p.BluRay.x264-iNGOT',
+           'file_name': 'Bored.to.Death.S01E03.Deleted.Scene.720p.BluRay.x264-iNGOT.mkv',
+           'size': 30779540,
+           'crc': '0ECBEBE8'},
+          'Bored.to.Death.S01E04.Deleted.Scene.720p.BluRay.x264-iNGOT.mkv':
+          {'release_name': 'Bored.to.Death.S01.EXTRAS.720p.BluRay.x264-iNGOT',
+           'file_name': 'Bored.to.Death.S01E04.Deleted.Scene.720p.BluRay.x264-iNGOT.mkv',
+           'size': 138052914,
+           'crc': 'E4E41C29'},
+          'Bored.to.Death.S01E08.Deleted.Scene.1.720p.BluRay.x264-iNGOT.mkv':
+          {'release_name': 'Bored.to.Death.S01.EXTRAS.720p.BluRay.x264-iNGOT',
+           'file_name': 'Bored.to.Death.S01E08.Deleted.Scene.1.720p.BluRay.x264-iNGOT.mkv',
+           'size': 68498554,
+           'crc': '085C6946'},
+          'Bored.to.Death.S01E08.Deleted.Scene.2.720p.BluRay.x264-iNGOT.mkv':
+          {'release_name': 'Bored.to.Death.S01.EXTRAS.720p.BluRay.x264-iNGOT',
+           'file_name': 'Bored.to.Death.S01E08.Deleted.Scene.2.720p.BluRay.x264-iNGOT.mkv',
+           'size': 45583011,
+           'crc': '7B822E59'}}),
+
+        # Looking for multi-file release when original releases where single-file
+        ('Fawlty.Towers.S01.720p.BluRay.x264-SHORTBREHD',
+         {'Fawlty.Towers.S01E01.720p.BluRay.x264-SHORTBREHD.mkv':
+          {'release_name': 'Fawlty.Towers.S01E01.720p.BluRay.x264-SHORTBREHD',
+           'file_name': 'Fawlty.Towers.S01E01.720p.BluRay.x264-SHORTBREHD.mkv',
+           'size': 1559430969, 'crc': 'B334CBEA'},
+          'Fawlty.Towers.S01E02.720p.BluRay.x264-SHORTBREHD.mkv':
+          {'release_name': 'Fawlty.Towers.S01E02.720p.BluRay.x264-SHORTBREHD',
+           'file_name': 'Fawlty.Towers.S01E02.720p.BluRay.x264-SHORTBREHD.mkv',
+           'size': 1168328357, 'crc': '156FDC0E'},
+          'Fawlty.Towers.S01E03.720p.BluRay.x264-SHORTBREHD.mkv':
+          {'release_name': 'Fawlty.Towers.S01E03.720p.BluRay.x264-SHORTBREHD',
+           'file_name': 'Fawlty.Towers.S01E03.720p.BluRay.x264-SHORTBREHD.mkv',
+           'size': 1559496197, 'crc': '5EAAAEC3'},
+          'Fawlty.Towers.S01E04.720p.BluRay.x264-SHORTBREHD.mkv':
+          {'release_name': 'Fawlty.Towers.S01E04.720p.BluRay.x264-SHORTBREHD',
+           'file_name': 'Fawlty.Towers.S01E04.720p.BluRay.x264-SHORTBREHD.mkv',
+           'size': 1560040644, 'crc': '4AAB7B8F'},
+          'Fawlty.Towers.S01E05.720p.BluRay.x264-SHORTBREHD.mkv':
+          {'release_name': 'Fawlty.Towers.S01E05.720p.BluRay.x264-SHORTBREHD',
+           'file_name': 'Fawlty.Towers.S01E05.720p.BluRay.x264-SHORTBREHD.mkv',
+           'size': 1559747905, 'crc': '40A2568C'},
+          'Fawlty.Towers.S01E06.720p.BluRay.x264-SHORTBREHD.mkv':
+          {'release_name': 'Fawlty.Towers.S01E06.720p.BluRay.x264-SHORTBREHD',
+           'file_name': 'Fawlty.Towers.S01E06.720p.BluRay.x264-SHORTBREHD.mkv',
+           'size': 1559556862, 'crc': '9FAB6E5F'}}),
+
+        # Looking for single episode from multi-file release
+        ('Bored.to.Death.S01E04.Deleted.Scene.720p.BluRay.x264-iNGOT.mkv',
+         {'Bored.to.Death.S01E04.Deleted.Scene.720p.BluRay.x264-iNGOT.mkv':
+          {'release_name': 'Bored.to.Death.S01.EXTRAS.720p.BluRay.x264-iNGOT',
+           'file_name': 'Bored.to.Death.S01E04.Deleted.Scene.720p.BluRay.x264-iNGOT.mkv',
+           'size': 138052914,
+           'crc': 'E4E41C29'}}),
+
+        # Looking for single non-episode file from multi-file release
+        ('Bored.to.Death.S01.Making.of.720p.BluRay.x264-iNGOT',
+         {'Bored.to.Death.S01.Making.of.720p.BluRay.x264-iNGOT.mkv':
+          {'release_name': 'Bored.to.Death.S01.EXTRAS.720p.BluRay.x264-iNGOT',
+           'file_name': 'Bored.to.Death.S01.Making.of.720p.BluRay.x264-iNGOT.mkv',
+           'size': 779447228,
+           'crc': '8C8C0AE7'}}),
+
+        # # Non-scene release
+        ('Foo.2015.1080p.Bluray.DD5.1.x264-DON.mkv', {}),
+    ),
+)
+@pytest.mark.asyncio
+async def test_release_files(release_name, exp_return_value, store_response):
+    assert await scene.release_files(release_name) == exp_return_value
--- upsies/utils/scene/__init__.py
+++ upsies/utils/scene/__init__.py
@@ -97,3 +97,58 @@ async def is_scene_release(release):
         return SceneCheckResult.unknown
 
     return SceneCheckResult.false
+
+
+async def release_files(release_name):
+    """
+    Map release file names to file information
+
+    This function uses :class:`~predb.PredDb` for searching and
+    :class:`~.srrdb.SrrDbApi` to get the file information.
+
+    :param str release_name: Exact name of the release
+    """
+    files = await _SRRDB.release_files(release_name)
+    if files:
+        return files
+    else:
+        _log.debug('No such release: %r', release_name)
+        files = {}
+
+    # If scene released "Foo.S01E0{1,2,3,...}.720p.BluRay-BAR" and we're
+    # searching for "Foo.S01.720p.BluRay-BAR", we might not get any results. But
+    # we can get release names of individual episodes by searching for the
+    # season pack, and then we can call release_files() for each episode.
+    release = utils.release.ReleaseInfo(release_name)
+    if release['type'] is ReleaseType.season:
+        query = SceneQuery.from_release(release)
+        results = await _PREDB.search(query)
+        if results:
+            files = await asyncio.gather(
+                *(_SRRDB.release_files(result) for result in results)
+            )
+            # Join sequence of dictionaries into single dictionary
+            files = {fname:f for files_ in files for fname,f in files_.items()}
+            _log.debug('Season pack from multiple episode releases: %r', files)
+
+    # If scene released season pack (or any other multi-file thing) and we're
+    # searching for a single episode, we might not get any results. Search for
+    # the season pack to get all files.
+    elif release['type'] is ReleaseType.episode:
+        # Remove single episodes from seasons
+        release['episodes'].remove_specific_episodes()
+        results = await _PREDB.search(SceneQuery.from_release(release))
+        if len(results) == 1:
+            _log.debug('Getting files from single result: %r', results[0])
+            files = await _SRRDB.release_files(results[0])
+
+    # Go through all files and find the exact release name we're looking for.
+    # Don't do this exclusively for episodes because not all multi-file releases
+    # are a list of episodes (e.g. extras may not contain any "Exx").
+    for file_name, file_info in files.items():
+        if utils.fs.strip_extension(release_name) == utils.fs.strip_extension(file_name):
+            files = {file_name: file_info}
+            _log.debug('Single file from season pack release: %r', files)
+            break
+
+    return files
