6e9499b3d4bd6256bb1aee49e223e1aefab8b6e2
plotski <plotski@example.org>
2021-02-16 19:25:11 +0100

    Add SceneCheckJob

---
+++ tests/jobs_test/scene_check_job_test.py
@@ -0,0 +1,257 @@
+import asyncio
+from unittest.mock import Mock, call
+
+import pytest
+
+from upsies import errors
+from upsies.jobs.scene import SceneCheckJob
+from upsies.utils.scene import SceneQuery
+from upsies.utils.types import SceneCheckResult
+
+
+class AsyncMock(Mock):
+    def __call__(self, *args, **kwargs):
+        async def coro(_sup=super()):
+            return _sup.__call__(*args, **kwargs)
+        return coro()
+
+
+@pytest.fixture
+async def make_SceneCheckJob(tmp_path):
+    def make_SceneCheckJob(content_path=tmp_path, ignore_cache=False):
+        return SceneCheckJob(
+            home_directory=tmp_path,
+            ignore_cache=ignore_cache,
+            content_path=content_path,
+        )
+    return make_SceneCheckJob
+
+
+def test_cache_id(make_SceneCheckJob):
+    job = make_SceneCheckJob(content_path='path/to/Foo/')
+    assert job.cache_id == ('Foo',)
+
+
+@pytest.mark.parametrize(
+    argnames='exc, msg, exp_exc, exp_msg',
+    argvalues=(
+        (None, None, None, None),
+        (errors.RequestError, 'Foo', None, None),
+        (errors.SceneError, 'Foo', None, None),
+        (TypeError, 'Foo', TypeError, 'Foo'),
+    ),
+    ids=lambda v: str(v),
+)
+@pytest.mark.asyncio
+async def test_catch_errors(exc, msg, exp_exc, exp_msg, make_SceneCheckJob, mocker):
+    async def coro():
+        if exc:
+            raise exc(msg)
+
+    job = make_SceneCheckJob()
+    await job._catch_errors(coro())
+
+    if exp_exc:
+        asyncio.get_event_loop().call_soon(job.finish)
+        with pytest.raises(exp_exc, match=rf'^{exp_msg}$'):
+            await job.wait()
+    else:
+        if not exc:
+            asyncio.get_event_loop().call_soon(job.finish)
+        await job.wait()
+        assert job.errors == ((exc(msg),) if exc else ())
+        assert job.is_finished
+
+
+@pytest.mark.asyncio
+async def test_execute(make_SceneCheckJob, mocker):
+    mocker.patch('upsies.jobs.scene.SceneCheckJob._find_release_name', AsyncMock())
+    job = make_SceneCheckJob()
+    job.execute()
+    asyncio.get_event_loop().call_soon(job.finish)
+    await job.wait()
+    assert job._find_release_name.call_args_list == [call()]
+
+
+@pytest.mark.asyncio
+async def test_find_release_name_gets_nonscene_release(make_SceneCheckJob, mocker):
+    is_scene_release_mock = mocker.patch('upsies.utils.scene.is_scene_release', AsyncMock(
+        return_value=SceneCheckResult.false,
+    ))
+    mocker.patch('upsies.jobs.scene.SceneCheckJob._finalize')
+    mocker.patch('upsies.jobs.scene.SceneCheckJob._verify_release', AsyncMock())
+    prompt_release_name = Mock()
+    job = make_SceneCheckJob(content_path='path/to/foo')
+    job.signal.register('prompt_release_name', prompt_release_name)
+    await job._find_release_name()
+    assert is_scene_release_mock.call_args_list == [call('path/to/foo')]
+    assert prompt_release_name.call_args_list == []
+    assert job._finalize.call_args_list == [
+        call(SceneCheckResult.false, exceptions=()),
+    ]
+    assert job._verify_release.call_args_list == []
+
+@pytest.mark.parametrize('is_scene_release', (SceneCheckResult.true, SceneCheckResult.unknown))
+@pytest.mark.asyncio
+async def test_find_release_name_finds_no_results(is_scene_release, make_SceneCheckJob, mocker):
+    is_scene_release_mock = mocker.patch('upsies.utils.scene.is_scene_release', AsyncMock(
+        return_value=is_scene_release,
+    ))
+    search_mock = mocker.patch('upsies.utils.scene.search', AsyncMock(
+        return_value=(),
+    ))
+    mocker.patch('upsies.jobs.scene.SceneCheckJob._finalize')
+    mocker.patch('upsies.jobs.scene.SceneCheckJob._verify_release', AsyncMock())
+    prompt_release_name = Mock()
+    job = make_SceneCheckJob(content_path='path/to/foo')
+    job.signal.register('prompt_release_name', prompt_release_name)
+    await job._find_release_name()
+    assert is_scene_release_mock.call_args_list == [call('path/to/foo')]
+    assert search_mock.call_args_list == [call(SceneQuery('foo'))]
+    assert prompt_release_name.call_args_list == []
+    assert job._finalize.call_args_list == [
+        call(SceneCheckResult.unknown, exceptions=()),
+    ]
+    assert job._verify_release.call_args_list == []
+
+@pytest.mark.parametrize('is_scene_release', (SceneCheckResult.true, SceneCheckResult.unknown))
+@pytest.mark.asyncio
+async def test_find_release_name_finds_single_result(is_scene_release, make_SceneCheckJob, mocker):
+    is_scene_release_mock = mocker.patch('upsies.utils.scene.is_scene_release', AsyncMock(
+        return_value=is_scene_release,
+    ))
+    search_mock = mocker.patch('upsies.utils.scene.search', AsyncMock(
+        return_value=('Mock.Release.Foo-BAR',),
+    ))
+    mocker.patch('upsies.jobs.scene.SceneCheckJob._finalize')
+    mocker.patch('upsies.jobs.scene.SceneCheckJob._verify_release', AsyncMock())
+    prompt_release_name = Mock()
+    job = make_SceneCheckJob(content_path='path/to/foo')
+    job.signal.register('prompt_release_name', prompt_release_name)
+    await job._find_release_name()
+    assert is_scene_release_mock.call_args_list == [call('path/to/foo')]
+    assert search_mock.call_args_list == [call(SceneQuery('foo'))]
+    assert prompt_release_name.call_args_list == []
+    assert job._finalize.call_args_list == []
+    assert job._verify_release.call_args_list == [
+        call('Mock.Release.Foo-BAR'),
+    ]
+
+@pytest.mark.parametrize('is_scene_release', (SceneCheckResult.true, SceneCheckResult.unknown))
+@pytest.mark.asyncio
+async def test_find_release_name_finds_parsed_release_name_in_result(is_scene_release, make_SceneCheckJob, mocker):
+    is_scene_release_mock = mocker.patch('upsies.utils.scene.is_scene_release', AsyncMock(
+        return_value=is_scene_release,
+    ))
+    search_mock = mocker.patch('upsies.utils.scene.search', AsyncMock(
+        return_value=('foo', 'bar', 'baz'),
+    ))
+    mocker.patch('upsies.jobs.scene.SceneCheckJob._finalize')
+    mocker.patch('upsies.jobs.scene.SceneCheckJob._verify_release', AsyncMock())
+    prompt_release_name = Mock()
+    job = make_SceneCheckJob(content_path='path/to/foo')
+    job.signal.register('prompt_release_name', prompt_release_name)
+    await job._find_release_name()
+    assert is_scene_release_mock.call_args_list == [call('path/to/foo')]
+    assert search_mock.call_args_list == [call(SceneQuery('foo'))]
+    assert prompt_release_name.call_args_list == []
+    assert job._finalize.call_args_list == []
+    assert job._verify_release.call_args_list == [
+        call('foo'),
+    ]
+
+@pytest.mark.parametrize('is_scene_release', (SceneCheckResult.true, SceneCheckResult.unknown))
+@pytest.mark.asyncio
+async def test_find_release_name_finds_multiple_results(is_scene_release, make_SceneCheckJob, mocker):
+    is_scene_release_mock = mocker.patch('upsies.utils.scene.is_scene_release', AsyncMock(
+        return_value=is_scene_release,
+    ))
+    search_mock = mocker.patch('upsies.utils.scene.search', AsyncMock(
+        return_value=('Mock.Release.Foo-BAR', 'Mick.Release.Foo-BAR'),
+    ))
+    mocker.patch('upsies.jobs.scene.SceneCheckJob._finalize')
+    mocker.patch('upsies.jobs.scene.SceneCheckJob._verify_release', AsyncMock())
+    prompt_release_name = Mock()
+    job = make_SceneCheckJob(content_path='path/to/foo')
+    job.signal.register('prompt_release_name', prompt_release_name)
+    await job._find_release_name()
+    assert is_scene_release_mock.call_args_list == [call('path/to/foo')]
+    assert search_mock.call_args_list == [call(SceneQuery('foo'))]
+    assert prompt_release_name.call_args_list == [
+        call(('Mock.Release.Foo-BAR', 'Mick.Release.Foo-BAR')),
+    ]
+    assert job._finalize.call_args_list == []
+    assert job._verify_release.call_args_list == []
+
+
+@pytest.mark.asyncio
+async def test_user_selected_release_name(make_SceneCheckJob, mocker):
+    mocker.patch('upsies.jobs.scene.SceneCheckJob._verify_release', AsyncMock())
+    job = make_SceneCheckJob()
+    job.user_selected_release_name('mock.release.name')
+    asyncio.get_event_loop().call_soon(job.finish)
+    await job.wait()
+    assert job._verify_release.call_args_list == [call('mock.release.name')]
+
+
+@pytest.mark.asyncio
+async def test_verify_release(make_SceneCheckJob, mocker):
+    mocker.patch('upsies.jobs.scene.SceneCheckJob._finalize')
+    verify_release_mock = mocker.patch('upsies.utils.scene.verify_release', AsyncMock(
+        return_value=(SceneCheckResult.true, ('error1', 'error2')),
+    ))
+    job = make_SceneCheckJob()
+    await job._verify_release('mock.release.name')
+    assert verify_release_mock.call_args_list == [
+        call(job._content_path, 'mock.release.name'),
+    ]
+    assert job._finalize.call_args_list == [
+        call(SceneCheckResult.true, ('error1', 'error2')),
+    ]
+
+
+def test_finalize_ignores_SceneMissingFileError(make_SceneCheckJob, mocker):
+    prompt_is_scene_release = Mock()
+    job = make_SceneCheckJob()
+    job.signal.register('prompt_is_scene_release', prompt_is_scene_release)
+    job._finalize(
+        'mock scene check result',
+        (
+            errors.SceneError('foo'),
+            errors.SceneMissingFileError('bar'),
+            errors.SceneFileSizeError('baz', 123, 456),
+            errors.SceneMissingFileError('bam'),
+        ),
+    )
+    assert job.errors == (
+        errors.SceneError('foo'),
+        errors.SceneFileSizeError('baz', 123, 456),
+    )
+    assert prompt_is_scene_release.call_args_list == []
+    assert job.is_finished
+
+def test_finalize_triggers_prompt_without_errors(make_SceneCheckJob, mocker):
+    prompt_is_scene_release = Mock()
+    job = make_SceneCheckJob()
+    job.signal.register('prompt_is_scene_release', prompt_is_scene_release)
+    job._finalize('mock scene check result', ())
+    assert job.errors == ()
+    assert prompt_is_scene_release.call_args_list == [
+        call('mock scene check result'),
+    ]
+    assert not job.is_finished
+
+@pytest.mark.parametrize(
+    argnames='is_scene_release, exp_msg',
+    argvalues=(
+        (SceneCheckResult.true, 'Scene release'),
+        (SceneCheckResult.false, 'Not a scene release'),
+        (SceneCheckResult.unknown, 'May be a scene release'),
+    ),
+    ids=lambda v: str(v),
+)
+def test_user_decided(is_scene_release, exp_msg, make_SceneCheckJob, mocker):
+    job = make_SceneCheckJob()
+    job.user_decided(is_scene_release)
+    assert job.output == (exp_msg,)
+    assert job.is_finished
--- upsies/jobs/scene.py
+++ upsies/jobs/scene.py
@@ -75,3 +75,133 @@ class SceneSearchJob(JobBase):
             self.signal.emit('search_results', results)
         finally:
             self.finish()
+
+
+class SceneCheckJob(JobBase):
+    """
+    Verify scene release name and content
+
+    This job adds the following signals to the :attr:`~.JobBase.signal`
+    attribute:
+
+        ``prompt_release_name``
+            Emitted if the user must pick a release name from multiple search
+            results. Registered callbacks get a sequence of release names as
+            positional argument.
+
+        ``prompt_is_scene_release``
+            Emitted after we made our best guess and the user must approve or
+            override it. Registered callbacks get a
+            :class:`~.types.SceneCheckResult` enum as a positional argument.
+
+        ``checked``
+            Emitted after the user decided whether it's a scene release or not.
+            Registered callbacks get a :class:`~.utils.types.SceneCheckResult`
+            enum as a positional argument.
+    """
+
+    name = 'scene-check'
+    label = 'Scene Check'
+    hidden = False
+
+    @cached_property
+    def cache_id(self):
+        """Final segment of the `content_path` argument to :meth:`initialize`"""
+        return (fs.basename(self._content_path),)
+
+    def initialize(self, *, content_path):
+        """
+        Set internal state
+
+        :param content_path: Path to video file or directory that contains a
+            video file or release name
+        """
+        self._content_path = content_path
+        self._release_name = fs.strip_extension(fs.basename(content_path))
+        self.signal.add('prompt_release_name')
+        self.signal.add('prompt_is_scene_release')
+        self.signal.add('checked')
+
+    async def _catch_errors(self, coro):
+        try:
+            return await coro
+        except errors.RequestError as e:
+            self.error(e, finish=True)
+        except errors.SceneError as e:
+            self.error(e, finish=True)
+        except BaseException as e:
+            self.exception(e)
+
+    def execute(self):
+        """Start asynchronous background worker task"""
+        asyncio.ensure_future(
+            self._catch_errors(
+                self._find_release_name()
+            )
+        )
+
+    async def _find_release_name(self):
+        is_scene_release = await scene.is_scene_release(self._content_path)
+        if is_scene_release is types.SceneCheckResult.false:
+            _log.debug('Not a scene release: %r', self._content_path)
+            self._finalize(types.SceneCheckResult.false, exceptions=())
+        else:
+            # Find specific release name. If there are multiple search results,
+            # ask the user to pick one.
+            query = scene.SceneQuery.from_string(self._content_path)
+            results = await scene.search(query)
+            if not results:
+                _log.debug('No search results: %r', self._content_path)
+                self._finalize(types.SceneCheckResult.unknown, exceptions=())
+            elif len(results) == 1:
+                _log.debug('Auto-picking only search result: %r', results[0])
+                release_name = results[0]
+                await self._verify_release(release_name)
+            elif self._release_name in results:
+                _log.debug('Auto-picking from search results: %r', self._release_name)
+                await self._verify_release(self._release_name)
+            else:
+                _log.debug('Prompting for release name: %r', results)
+                self.signal.emit('prompt_release_name', results)
+
+    def user_selected_release_name(self, release_name):
+        """
+        Must be called by the UI when the user picked a release name from search
+        results
+        """
+        _log.debug('User selected release name: %r', release_name)
+        asyncio.ensure_future(
+            self._catch_errors(
+                self._verify_release(release_name)
+            )
+        )
+
+    async def _verify_release(self, release_name):
+        _log.debug('Verifying release: %r', self._content_path)
+        is_scene_release, exceptions = await scene.verify_release(self._content_path, release_name)
+        self._finalize(is_scene_release, exceptions)
+
+    def _finalize(self, is_scene_release, exceptions=()):
+        _log.debug('Handling result: %r: %r', is_scene_release, exceptions)
+        for e in exceptions:
+            if not isinstance(e, errors.SceneMissingFileError):
+                self.error(e)
+
+        if not exceptions:
+            self.signal.emit('prompt_is_scene_release', is_scene_release)
+        else:
+            self.finish()
+
+    def user_decided(self, is_scene_release):
+        """
+        Must be called by the UI when the user decided whether this is a scene
+        release or not
+        """
+        _log.debug('User decided: %r', is_scene_release)
+        if is_scene_release is types.SceneCheckResult.true:
+            self.send('Scene release')
+        elif is_scene_release is types.SceneCheckResult.false:
+            self.send('Not a scene release')
+        else:
+            self.send('May be a scene release')
+        self.finish()
