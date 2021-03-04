eea5105906d0b1ef67a353c5083b4de9d6298bcc
plotski <plotski@example.org>
2021-03-04 12:06:50 +0100

    Don't show warnings of finished jobs

--- upsies/uis/tui/jobwidgets/base.py
+++ upsies/uis/tui/jobwidgets/base.py
@@ -45,7 +45,7 @@ class JobWidgetBase(abc.ABC):
                 ),
                 # Warnings
                 ConditionalContainer(
-                    filter=Condition(lambda: bool(self.job.warnings)),
+                    filter=Condition(lambda: not self.job.is_finished and bool(self.job.warnings)),
                     content=self.warnings_widget,
                 ),
                 # Errors
