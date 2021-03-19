4d56fac0e1b00b80a6dfe2a9479eaaf12822bb3f
plotski <plotski@example.org>
2021-03-19 11:29:58 +0100

    Make custom module available in jobs package

--- upsies/jobs/__init__.py
+++ upsies/jobs/__init__.py
@@ -3,5 +3,5 @@ Connect the user interface to the engine room
 """
 
 from .base import JobBase, QueueJobBase  # isort:skip
-from . import (config, dialog, imghost, mediainfo, release_name, scene,
+from . import (config, custom, dialog, imghost, mediainfo, release_name, scene,
                screenshots, submit, torrent, webdb)
