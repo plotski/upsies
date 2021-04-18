4be0521f71c1e34222d2e85c5e299d5d09b601ba
plotski <plotski@example.org>
2021-04-18 12:10:10 +0200

    Improve docstrings of application_setup/shutdown()

--- upsies/__init__.py
+++ upsies/__init__.py
@@ -7,13 +7,21 @@ __author_email__ = 'plotski@example.org'
 
 
 def application_setup(config):
-    """This function should be called by the UI ASAP when `config` is available"""
+    """
+    This function should be called by the UI ASAP when `config` is available
+
+    :param config: :class:`~.configfiles.ConfigFiles` instance
+    """
     from .utils import http
     http.cache_directory = config['config']['main']['tmp_directory']
 
 
 def application_shutdown(config):
-    """This function should be called by the UI before the applicatin terminates"""
+    """
+    This function should be called by the UI before the applicatin terminates
+
+    :param config: :class:`~.configfiles.ConfigFiles` instance
+    """
     from .utils import fs, http
     http.close()
     fs.limit_directory_size(
