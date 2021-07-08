7ce8db2c9931605eecd40dd25794210f7fd2f95f
plotski <plotski@example.org>
2021-07-08 15:46:37 +0200

    Add tui.utils.is_tty()

--- tests/uis_test/tui_test/tui_utils_test.py
+++ tests/uis_test/tui_test/tui_utils_test.py
@@ -1,7 +1,57 @@
 import asyncio
 from unittest.mock import Mock, call
 
+import pytest
+
 from upsies.uis.tui import utils
+
+
+@pytest.mark.parametrize(
+    argnames='stdin_isatty, stdout_isatty, stderr_isatty',
+    argvalues=(
+        (False, False, False),
+        (False, False, True),
+        (False, True, False),
+        (False, True, True),
+        (True, False, False),
+        (True, False, True),
+        (True, True, False),
+        (True, True, True),
+    ),
+)
+@pytest.mark.parametrize(
+    argnames='stdin, stdout, stderr',
+    argvalues=(
+        (None, None, None),
+        (None, None, Mock()),
+        (None, None, Mock()),
+        (None, Mock(), None),
+        (None, Mock(), Mock()),
+        (Mock(), None, None),
+        (Mock(), None, Mock()),
+        (Mock(), Mock(), None),
+        (Mock(), Mock(), Mock()),
+    ),
+)
+def test_is_tty(stdin, stdout, stderr, stdin_isatty, stdout_isatty, stderr_isatty, mocker):
+    if stdin:
+        stdin.isatty = lambda: stdin_isatty
+    if stdout:
+        stdout.isatty = lambda: stdout_isatty
+    if stderr:
+        stderr.isatty = lambda: stderr_isatty
+    mocker.patch('sys.stdin', stdin)
+    mocker.patch('sys.stdout', stdout)
+    mocker.patch('sys.stderr', stderr)
+
+    if not stdin or not stdin.isatty():
+        exp_is_tty = False
+    elif (stdout and stdout.isatty()) or (stderr and stderr.isatty()):
+        exp_is_tty = True
+    else:
+        exp_is_tty = False
+
+    assert utils.is_tty() is exp_is_tty
 
 
 def test_Throbber_active_argument():
--- upsies/uis/tui/utils.py
+++ upsies/uis/tui/utils.py
@@ -1,8 +1,24 @@
 import asyncio
 import itertools
+import sys
 
 import logging  # isort:skip
 _log = logging.getLogger(__name__)
+
+
+def is_tty():
+    """Whether we live in a terminal and can interacte with the user"""
+    # As long as we have input and output, we have user interaction. sys.stdout
+    # may be redirected, but prompt-toolkit will print to sys.stderr instead.
+    if not sys.stdin or (not sys.stdout and not sys.stderr):
+        return False
+    else:
+        if sys.stdin.isatty():
+            if sys.stdout and sys.stdout.isatty():
+                return True
+            elif sys.stderr and sys.stderr.isatty():
+                return True
+        return False
 
 
 class Throbber:
