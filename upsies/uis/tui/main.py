7f124a2135da8bb2cbb2fa6880e32ea4080095c8
plotski <plotski@example.org>
2021-07-19 12:10:32 +0200

    WTF: Add debugging argument

--- upsies/uis/tui/main.py
+++ upsies/uis/tui/main.py
@@ -13,6 +13,10 @@ def main(args=None):
 
 
 def _main(args=None):
+    if sys.argv[1] == 'wtf':
+        _wtf()
+        exit(0)
+
     try:
         if utils.is_tty():
             from .tui import TUI
@@ -62,3 +66,68 @@ def _main(args=None):
                     break
 
         return exit_code
+
+
+def _wtf():
+    import os
+    from ... import utils
+
+    print('$PATH:', os.environ['PATH'].split(':'))
+    for path in os.environ['PATH'].split(':'):
+        mipath = os.path.join(path, 'mediainfo')
+        print('Looking for mediainfo in', path)
+        if os.path.exists(mipath):
+            print('mediainfo found: ', mipath)
+            break
+    else:
+        print('No mediainfo found!')
+        exit(1)
+
+    print('##################################################')
+
+    micmd = ('mediainfo', '--version')
+    print(f'Running: {micmd}:')
+    print(utils.subproc.run(micmd))
+
+    print('##################################################')
+
+    micmd = ('mediainfo', '--Output=JSON', sys.argv[2])
+    print(f'Running: {micmd}:')
+    print(utils.subproc.run(micmd))
+
+    print('##################################################')
+
+    print(f'Running: utils.video._run_mediainfo({sys.argv[2]!r}, \'--Output=JSON\'):')
+    print(utils.video._run_mediainfo(sys.argv[2], '--Output=JSON'))
+
+    print('##################################################')
+
+    print(f'Running: utils.video._run_mediainfo({sys.argv[2]!r}, \'--Output=JSON\'):')
+    print(utils.video._run_mediainfo(sys.argv[2], '--Output=JSON'))
+
+    print('##################################################')
+
+    print(f'Running: utils.video._tracks({sys.argv[2]!r}):')
+    print(utils.video._tracks(sys.argv[2]))
+
+    print('##################################################')
+
+    print(f'Running: utils.video.tracks({sys.argv[2]!r}):')
+    print(utils.video.tracks(sys.argv[2]))
+
+    print('##################################################')
+
+    print(f'Running: utils.video.default_track({sys.argv[2]!r}):')
+    print(utils.video.default_track('video', sys.argv[2]))
+
+    print('##################################################')
+
+    print(f'Running: utils.video.width({sys.argv[2]!r}):')
+    print(utils.video.width(sys.argv[2]))
+
+    print('##################################################')
+
+    print(f'Running: utils.video.audio_format({sys.argv[2]!r}):')
+    print(utils.video.audio_format(sys.argv[2]))
+
+    
