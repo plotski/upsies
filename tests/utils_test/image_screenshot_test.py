7b7f44fe0086bda0e257600ebc6e88eea846cd41
plotski <plotski@example.org>
2021-07-26 11:49:09 +0200

    Deduplicate utils.image test

--- tests/utils_test/image_screenshot_test.py
+++ tests/utils_test/image_screenshot_test.py
@@ -9,20 +9,6 @@ from upsies.utils import image, video
 
 
 @pytest.mark.parametrize(
-    argnames='os, exp_executable',
-    argvalues=(
-        ('linux', 'ffmpeg'),
-        ('windows', 'ffmpeg.exe'),
-        ('plan9', 'ffmpeg'),
-    ),
-    ids=lambda v: str(v),
-)
-def test_ffmpeg_executable(os, exp_executable, mocker):
-    mocker.patch('upsies.utils.os_family', return_value=os)
-    assert image._ffmpeg_executable() == exp_executable
-
-
-@pytest.mark.parametrize(
     argnames='video_file, timestamp, screenshot_file, exp_args',
     argvalues=(
         ('video.mkv', '123', 'out.png',
