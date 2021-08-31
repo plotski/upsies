cbe04df467e51f068a760072cdf377e78c5704f0
plotski <plotski@example.org>
2021-08-31 16:11:41 +0200

    utils.update: Also report new prerelease if appropriate
    If the installed version is a prerelease, check for a newer prerelease.
    
    If the installed version is not a prerelease, only check for a newer stable
    release.

--- tests/utils_test/update_test.py
+++ tests/utils_test/update_test.py
@@ -1,5 +1,6 @@
 from unittest.mock import Mock, call
 
+import packaging
 import pytest
 
 from upsies import __project_name__
@@ -30,6 +31,36 @@ def test_fix_version(version, exp_version):
 
 
 @pytest.mark.parametrize(
+    argnames='current, release, prerelease, exp_latest_version',
+    argvalues=(
+        ('2021.06.10', '2021.06.10', '2021.06.10', '2021.06.10'),
+        ('2021.06.10', '2021.06.15', '2021.06.20alpha', '2021.06.15'),
+
+        ('2021.06.20alpha', '2021.06.15', '2021.06.20alpha', '2021.06.20alpha'),
+        ('2021.06.20alpha', '2021.06.15', '2021.06.25alpha', '2021.06.25alpha'),
+
+        ('2021.06.20alpha', '2021.06.15', '2021.06.20alpha1', '2021.06.20alpha1'),
+        ('2021.06.20alpha2', '2021.06.15', '2021.06.25alpha3', '2021.06.25alpha3'),
+    ),
+)
+@pytest.mark.asyncio
+async def test_get_latest_version(current, release, prerelease, exp_latest_version, mocker):
+    get_latest_release_mock = mocker.patch('upsies.utils.update._get_latest_release_version',
+                                           AsyncMock(return_value=release))
+    get_latest_prerelease_mock = mocker.patch('upsies.utils.update._get_latest_prerelease_version',
+                                              AsyncMock(return_value=prerelease))
+    mocker.patch('upsies.utils.update.__version__', current)
+    latest_version = await update.get_latest_version()
+    assert latest_version == exp_latest_version
+    if packaging.version.parse(current).is_prerelease:
+        assert get_latest_release_mock.call_args_list == []
+        assert get_latest_prerelease_mock.call_args_list == [call()]
+    else:
+        assert get_latest_release_mock.call_args_list == [call()]
+        assert get_latest_prerelease_mock.call_args_list == []
+
+
+@pytest.mark.parametrize(
     argnames='versions, exp_latest_version',
     argvalues=(
         (['2021.6.20', '2021.8.10', '2023.4.7'], '2023.4.7'),
@@ -37,24 +68,53 @@ def test_fix_version(version, exp_version):
     ),
 )
 @pytest.mark.asyncio
-async def test_get_latest_version_returns_last_version(versions, exp_latest_version, mocker):
+async def test_get_latest_release_version(versions, exp_latest_version, mocker):
     response_mock = Mock(
         json=Mock(return_value={
             'releases': {v: f'info about {v!r}' for v in versions},
         }),
     )
     get_mock = mocker.patch('upsies.utils.http.get', AsyncMock(return_value=response_mock))
-
-    latest_version = await update.get_latest_version()
-
+    latest_version = await update._get_latest_release_version()
     if exp_latest_version:
         assert latest_version == update._fix_version(exp_latest_version)
     else:
         assert latest_version is None
-
-    exp_url = update._INFO_URL_PATTERN.format(project_name=__project_name__)
     assert get_mock.call_args_list == [
-        call(exp_url, cache=True, max_cache_age=update._MAX_CACHE_AGE),
+        call(
+            url=update._PYPI_URL,
+            timeout=update._REQUEST_TIMEOUT,
+            cache=True,
+            max_cache_age=update._MAX_CACHE_AGE,
+        ),
+    ]
+
+
+@pytest.mark.parametrize(
+    argnames='init_file_string, exp_latest_version',
+    argvalues=(
+        ("foo\n__version__='2021.06.20'\nbar\n", '2021.06.20'),
+        ('foo\n__version__="2021.06.20"\nbar\n', '2021.06.20'),
+        ("foo\n__version__ = '2021.06.20'\nbar\n", '2021.06.20'),
+        ('foo\n__version__ = "2021.06.20"\nbar\n', '2021.06.20'),
+        ("foo\n__venison__='2021.06.20'\nbar\n", None),
+    ),
+)
+@pytest.mark.asyncio
+async def test_get_latest_prerelease_version(init_file_string, exp_latest_version, mocker):
+    get_mock = mocker.patch('upsies.utils.http.get', AsyncMock(return_value=init_file_string))
+    latest_version = await update._get_latest_prerelease_version()
+    if exp_latest_version:
+        assert latest_version == exp_latest_version
+    else:
+        assert latest_version is None
+    assert get_mock.call_args_list == [
+        call(
+            url=update._REPO_URL,
+            timeout=update._REQUEST_TIMEOUT,
+            cache=True,
+            max_cache_age=update._MAX_CACHE_AGE,
+        ),
     ]
 
 
--- upsies/utils/update.py
+++ upsies/utils/update.py
@@ -4,6 +4,8 @@ Find most recent version
 
 import re
 
+from packaging.version import parse as parse_version
+
 from .. import __project_name__, __version__
 from . import http
 
@@ -11,8 +13,11 @@ import logging  # isort:skip
 _log = logging.getLogger(__name__)
 
 
-_INFO_URL_PATTERN = 'https://pypi.org/pypi/{project_name}/json'
+_PYPI_URL = f'https://pypi.org/pypi/{__project_name__}/json'
+_REPO_URL = ('https://raw.githubusercontent.com/'
+             f'plotski/{__project_name__}/master/{__project_name__}/__init__.py')
 _MAX_CACHE_AGE = 3600  # 1 hour
+_REQUEST_TIMEOUT = 3
 
 
 def _fix_version(version):
@@ -25,26 +30,51 @@ def _fix_version(version):
 
 async def get_latest_version():
     """
-    Return the version of the latest release
+    Return the latest version
+
+    If the current version is a prerelease, get the latest version from source
+    code in the repository (e.g. GitHub).
+
+    If the current version is a stable release, get the latest version from the
+    package index (e.g. PyPI).
 
     :raise RequestError: if getting the latest version fails
     """
-    url = _INFO_URL_PATTERN.format(project_name=__project_name__)
-    response = await http.get(url, cache=True, max_cache_age=_MAX_CACHE_AGE)
+    if parse_version(__version__).is_prerelease:
+        return await _get_latest_prerelease_version()
+    else:
+        return await _get_latest_release_version()
+
+async def _get_latest_release_version():
+    response = await http.get(
+        url=_PYPI_URL,
+        timeout=_REQUEST_TIMEOUT,
+        cache=True,
+        max_cache_age=_MAX_CACHE_AGE,
+    )
     all_versions = tuple(response.json()['releases'])
     # PyPI should return the releases sorted by release date (latest last)
     if all_versions:
         return _fix_version(all_versions[-1])
 
+async def _get_latest_prerelease_version():
+    response = await http.get(
+        url=_REPO_URL,
+        timeout=_REQUEST_TIMEOUT,
+        cache=True,
+        max_cache_age=_MAX_CACHE_AGE,
+    )
+    match = re.search(r'^__version__\s*=\s*[\'"]([\w\.]+)[\'"]', response, flags=re.MULTILINE)
+    if match:
+        return match.group(1)
+
 
 async def get_update_message():
-    from packaging import version
     latest = await get_latest_version()
     current = __version__
     msg = f'Latest {__project_name__} version: {latest}'
-
-    latest_parsed = version.parse(latest)
-    current_parsed = version.parse(current)
+    latest_parsed = parse_version(latest)
+    current_parsed = parse_version(current)
     if current_parsed.is_prerelease:
         if latest_parsed > current_parsed:
             return msg
