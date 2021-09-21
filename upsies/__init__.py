6581e530ae4c432421592e65bb96982e564dacee
plotski <plotski@example.org>
2021-09-21 11:56:33 +0200

    Bump version

--- upsies/__init__.py
+++ upsies/__init__.py
@@ -1,7 +1,7 @@
 __project_name__ = 'upsies'
 __description__ = 'Media metadata aggregator'
 __homepage__ = 'https://upsies.readthedocs.io'
-__version__ = '2021.09.13alpha2'
+__version__ = '2021.09.21alpha'
 __author__ = 'plotski'
 __author_email__ = 'plotski@example.org'
 
