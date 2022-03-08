c727c1c8769172a71c1e284c8ab7c5e9d5bfc48f
plotski <plotski@example.org>
2022-03-08 17:07:52 +0100

    Bump version

--- upsies/__init__.py
+++ upsies/__init__.py
@@ -1,7 +1,7 @@
 __project_name__ = 'upsies'
 __description__ = 'Media metadata aggregator'
 __homepage__ = 'https://upsies.readthedocs.io'
-__version__ = '2022.03.07alpha'
+__version__ = '2022.03.08alpha'
 __author__ = 'plotski'
 __author_email__ = 'plotski@example.org'
 
