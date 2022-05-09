5ad2c255dee124a4f4e12a72b7190964f5095d64
plotski <plotski@example.org>
2022-05-09 13:07:11 +0200

    download-location: Improve documentation

--- upsies/uis/tui/commands/download_location.py
+++ upsies/uis/tui/commands/download_location.py
@@ -8,23 +8,33 @@ from .base import CommandBase
 
 class download_location(CommandBase):
     """
-    Hard-link existing files as TORRENT expects them and print their location
+    Find files from TORRENT, create links if necessary, and print their location
 
     1. Find files from TORRENT in the file system.
 
        1.1 Beneath each LOCATION, find a file with the same size as the one
            specified in TORRENT.
 
-       1.2 Keep the file with the most similar name.
+       1.2 If there are multiple size matches, pick the file with the most
+           similar name to the one specified in TORRENT.
 
-       1.3 Hash two pieces to confirm matching file content.
+       1.3 Hash some pieces to confirm the file content is what TORRENT expects.
 
-    2. If necessary, create hard links beneath the first LOCATION a matching
-       file was found and print that LOCATION. This is the download path you
-       should pass to your client when adding TORRENT.
+    2. Pick the first LOCATION where a matching file was found. This is the
+       download directory that should be used when adding TORRENT to a
+       BitTorrent client.
+
+    3. Make sure every matching file exists beneath the download directory with
+       the same relative path as in TORRENT. Try to create a hard link and
+       default to a symbolic link if this fails because the link source and
+       target are on different file systems.
+
+    4. Print the download directory (see 2.).
 
        If no matching files are found, print the location passed to --default or
        the first LOCATION if there is no --default.
+
+       This is the only output.
     """
 
     names = ('download-location', 'dloc')
