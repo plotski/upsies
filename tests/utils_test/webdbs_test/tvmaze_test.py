7662cb326b4825d339084f9d80b4446573bc74b7
plotski <plotski@example.org>
2022-07-14 12:02:06 +0200

    TvmazeApi: Update test

--- tests/utils_test/webdbs_test/tvmaze_test.py
+++ tests/utils_test/webdbs_test/tvmaze_test.py
@@ -383,7 +383,7 @@ async def test_poster_url(id, season, exp_url, api, store_response):
 @pytest.mark.parametrize(
     argnames=('id', 'exp_rating'),
     argvalues=(
-        (1259, 8.3),
+        (1259, 8.1),
         (35256, 7.9),
         (36072, 7.6),
         (None, None),
